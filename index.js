25dd2e0a14bdb59551c1379a888c12d4af6b7383
James Halliday <substack@gmail.com>
2011-02-12 01:21:34 -0800

    beginnings of the javascript frontend

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ var bigint = new ffi.Library(__dirname + '/build/default/libbigint', {
     fromString : [ 'uint32', [ 'string', 'uint32' ] ],
 });
 
-module.exports = function (num) {
-    if (typeof num === number) {
+module.exports = BigInt;
+
+function BigInt (num, base) {
+    if (!(this instanceof BigInt)) return new BigInt(num, base);
+    
+    if (typeof num === 'number') {
+        this.id = bigint.fromString(num.toString(), base);
     }
+    else if (typeof num === 'string') {
+        this.id = bigint.fromString(num, base || 10);
+    }
+    else {
+        this.id = bigint.create();
+    }
+}
+
+BigInt.prototype.inspect = function () {
+    return '<BigInt ' + this.toString(10) + '>';
 };
 
-var x = bigint.create();
-console.log('x.toString(10) = ' + bigint.toString(x, 10));
+BigInt.prototype.toString = function (base) {
+    return bigint.toString(this.id, base || 10);
+};
 
-var y = bigint.fromString('555', 10);
-console.log('y = ' + y);
-console.log('y.toString(10) = ' + bigint.toString(y, 10));
-
-bigint.destroy(x);
-bigint.destroy(y);
+BigInt.prototype.destroy = function (base) {
+    bigint.destroy(this.id);
+};
