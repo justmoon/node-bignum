416f5c18ff6a3984a0ad0b7878b7d51fd69eea15
James Halliday <substack@gmail.com>
2011-03-21 03:18:48 -0700

    random seed test passes

---
+++ test/seed.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var exec = require('child_process').exec;
+
+exports.rand = function () {
+    var to = setTimeout(function () {
+        assert.fail('never executed');
+    }, 5000);
+    
+    var cmd = 'node -e \'require("bigint").rand(1000).toString()\'';
+    exec(cmd, function (err1, r1) {
+        exec(cmd, function (err2, r2) {
+            clearTimeout(to);
+            
+            assert.ok(!err1);
+            assert.ok(!err2);
+            
+            assert.ok(r1.match(/^\d+\n$/));
+            assert.ok(r2.match(/^\d+\n$/));
+            
+            var n1 = parseInt(r1, 10);
+            var n2 = parseInt(r2, 10);
+            
+            assert.ok(n1 >= 0, 'n1 >= 0');
+            assert.ok(n2 >= 0, 'n2 >= 0');
+            assert.ok(n1 < 1000, 'n1 < 1000');
+            assert.ok(n2 < 1000, 'n2 < 1000');
+            
+            assert.ok(n1 != n2, 'n1 != n2');
+        })
+    });
+}
