9c54caf9c8c9e75104328e8acd8ce484d477ed19
Rod Vagg <rod@vagg.org>
2014-06-29 13:59:57 -0700

    dos2unix

--- test/wincrash.js
+++ test/wincrash.js
@@ -1,7 +1,7 @@
 var assert = require('assert'),
     BigNum = require('../');
 
-exports.windowsCrash = function () { 
+exports.windowsCrash = function () {
     var num = new BigNum(1234);
     assert.equal(num.toString(), '1234')
 }
